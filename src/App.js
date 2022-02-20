00ba0304be0ffa295a7ede5861472ff9fb5978dd
Radomir Vladislavić <vladislavic.rl@gmail.com>
2022-02-20 18:05:33 -0400

    template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.css";
+import React, { useEffect, useLayoutEffect, useState, useRef } from "react";
+import cytoscape from "cytoscape";
+import edgehandles from "cytoscape-edgehandles";
+cytoscape.use(edgehandles);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [cy, setCy] = useState(cytoscape());
+    const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+    useLayoutEffect(() => {
+        console.log("new cy");
+        const newCy = cytoscape({
+            container: document.getElementById("cy"),
+            elements: [
+                // list of graph elements to start with
+                {
+                    // node a
+                    data: { id: "a" },
+                },
+                {
+                    // node b
+                    data: { id: "b" },
+                },
+                {
+                    // edge ab
+                    data: { id: "ab", source: "a", target: "b" },
+                },
+            ],
+        });
+        newCy.edgehandles();
+        setCy(newCy);
+    }, []);
+
+    const [add, setAdd] = useState(false);
+    const toggleAdd = () => {
+        setAdd(!add);
+    };
+
+    useEffect(() => {
+        if (add) {
+            cy.on("tap", (e) => {
+                if (e.target === cy) {
+                    cy.add({
+                        data: { id: e.timeStamp },
+                        position: e.position,
+                    });
+                }
+            });
+        } else {
+            cy.removeListener("tap");
+        }
+    }, [add]);
+
+    const onMouseMove = (event) => {
+        setMousePosition({
+            x: event.screenX,
+            y: event.screenY,
+        });
+    };
+
+    return (
+        <div className="App">
+            <button onClick={toggleAdd}>
+                {add ? "add enabled" : "add disabled"}
+            </button>
+            <div id="cy" onMouseMove={onMouseMove}>
+            </div>
+        </div>
+    );
 }
 
 export default App;
