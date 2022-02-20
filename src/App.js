ea25444c3997c5e0ead8be1fc7553d966a24ab8e
Radomir Vladislavić <vladislavic.rl@gmail.com>
2022-02-20 18:54:08 -0400

    Botones node y edge

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./style.css";
-import React, { useEffect, useLayoutEffect, useState, useRef } from "react";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 import cytoscape from "cytoscape";
 import edgehandles from "cytoscape-edgehandles";
 cytoscape.use(edgehandles);
@@ -7,6 +7,10 @@ cytoscape.use(edgehandles);
 function App() {
     const [cy, setCy] = useState(cytoscape());
     const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+    const [addNode, setAddNode] = useState(false);
+    const [addEdge, setAddEdge] = useState(false);
+
     useLayoutEffect(() => {
         console.log("new cy");
         const newCy = cytoscape({
@@ -23,23 +27,48 @@ function App() {
                 },
                 {
                     // edge ab
-                    data: { id: "ab", source: "a", target: "b" },
+                    data: { id: "ab", source: "a", target: "b", weight: "10" },
                 },
+            ],
+            style: [
+                {
+                    selector: "node",
+                    style: {
+                        label: "data(id)",
+                        "text-valign": "center",
+                        "text-halign": "center",
+                    },
+                },
+                {
+                    selector: "edge",
+                    style: {
+                        label: "data(weight)",
+                        "target-arrow-shape": "triangle",
+                        "target-arrow-color": "black",
+                        "source-arrow-color": "black",
+                        "line-color": "#333",
+                        width: 1.5,
+                        "curve-style": "bezier",
+                    },
+                }
             ],
         });
         newCy.edgehandles();
         setCy(newCy);
     }, []);
 
-    const [add, setAdd] = useState(false);
-    const toggleAdd = () => {
-        setAdd(!add);
+    const setAllFalse = () => {
+        setAddNode(false);
+        setAddEdge(false);
     };
 
     useEffect(() => {
-        if (add) {
+        if (addNode) {
+            setAllFalse();
+            setAddNode(true);
             cy.on("tap", (e) => {
                 if (e.target === cy) {
+                    // text input
                     cy.add({
                         data: { id: e.timeStamp },
                         position: e.position,
@@ -49,22 +78,49 @@ function App() {
         } else {
             cy.removeListener("tap");
         }
-    }, [add]);
+    }, [addNode]);
+
+    useEffect(() => {
+        if(addEdge) {
+            setAllFalse();
+            setAddEdge(true);
+        }
+    }, [addEdge]);
+
+    
 
     const onMouseMove = (event) => {
         setMousePosition({
             x: event.screenX,
             y: event.screenY,
         });
+        // console.log(event.screenX, event.screenY);
+    };
+
+    // TODO: aqui le meten la matriz de adyacencia
+    const adjacencyMatrix = () => {
+        // lista de nodos
+        let nodes = cy.$("node");
+        nodes.forEach((element) => {
+            console.log(element.data());
+        });
+        // lista de aristas
+        let edges = cy.$("edge");
+        edges.forEach((element) => {
+            console.log(element.data());
+        });
     };
 
     return (
         <div className="App">
-            <button onClick={toggleAdd}>
-                {add ? "add enabled" : "add disabled"}
+            <button onClick={() => setAddNode(true)}>
+                {addNode ? "add node enabled" : "add node disabled"}
             </button>
-            <div id="cy" onMouseMove={onMouseMove}>
-            </div>
+            <button onClick={() => setAddEdge(true)}>
+                {addEdge ? "add edge enabled" : "add edge disabled"}
+            </button>
+            <button onClick={adjacencyMatrix}>data</button>
+            <div id="cy" onMouseMove={onMouseMove}></div>
         </div>
     );
 }
