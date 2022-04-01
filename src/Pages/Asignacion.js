a4bfbf8ef30a2fb130e84e1fd6c9daa6db6c64ba
Radomir Vladislavić <vladislavic.rl@gmail.com>
2022-04-01 00:20:25 -0400

    onclick

--- src/Pages/Asignacion.js
+++ src/Pages/Asignacion.js
@@ -1,28 +1,32 @@
-import React from 'react';
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 import Graph from "../components/Graph/Graph";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
-import '../Styles/asignacion.css';
+import "../Styles/asignacion.css";
 import Button from "../components/Button/Button";
 import AsignacionTable from "../components/AsignacionTable/AsignacionTable";
-
+import asignacionAlgorithm from "../utils/algorithms/asignacion";
 
 const Asignacion = () => {
-
-  const [matrix, setMatrix] = useState([
+    const [matrix, setMatrix] = useState([
         ["", "", ""],
         ["", "", ""],
         ["", "", ""],
     ]);
 
-  return (
-    <div className="container">
-        <Header title="Algoritmo de Asignación" logo="" />
-        <AsignacionTable matrix={matrix} setMatrix={setMatrix}/>
-        <Footer btnText="Ejecutar Asignacion"  />
-    </div>
-  )
-}
+    const onClick = (e) => {
+        e.preventDefault();
+        asignacionAlgorithm(matrix, true);
+    }
+
+    return (
+        <div className="container">
+            <Header title="Algoritmo de Asignación" logo="" />
+            <AsignacionTable matrix={matrix} setMatrix={setMatrix} />
+            <Footer btnText="Ejecutar Asignacion"  onClick={onClick}/>
+        </div>
+    );
+};
 
 export default Asignacion;
