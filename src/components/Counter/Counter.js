faf5b578ae6d6875ca9d26482c5d4a9d043febe3
Radomir Vladislavić <vladislavic.rl@gmail.com>
2022-04-01 00:01:42 -0400

    Tabla para algoritmo de asignacion

---
+++ src/components/Counter/Counter.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Counter = ({ variable, setVariable, text }) => {
+    const increase = () => {
+        setVariable(variable => variable + 1);
+    };
+
+    const decrease = () => {
+        setVariable(variable => variable === 1 ? 1 : variable - 1);
+    };
+
+    return (
+        <div>
+            <span>{text}</span>
+            <button onClick={decrease}>-</button>
+            <span>{Number(variable) - 1}</span>
+            <button onClick={increase}>+</button>
+        </div>
+    );
+};
+
+export default Counter;
