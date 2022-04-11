fd532bb6f458f9dcb2eafc5a919e3b309678c084
Radomir Vladislavić <vladislavic.rl@gmail.com>
2022-04-11 11:58:04 -0400

    Importar y exportar en json

--- src/components/AsignacionTable/AsignacionTable.js
+++ src/components/AsignacionTable/AsignacionTable.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 import Counter from "../Counter/Counter";
+import Button from "../Button/Button";
 
 import "./AsignacionTable.css";
 
@@ -8,10 +9,18 @@ const AsignacionTable = ({ matrix, setMatrix }) => {
     const [rows, setRows] = useState(3);
     const [columns, setColumns] = useState(3);
 
+    const inputRef = useRef(null);
+
     const handleChange = (e, i, j) => {
-        setMatrix(matrix.map((elem, row) =>
-            row === i ? elem.map((oldValue, col) => col === j ? e.target.value : oldValue) : elem
-        ));
+        setMatrix(
+            matrix.map((elem, row) =>
+                row === i
+                    ? elem.map((oldValue, col) =>
+                          col === j ? e.target.value : oldValue
+                      )
+                    : elem
+            )
+        );
     };
 
     useEffect(() => {
@@ -34,6 +43,45 @@ const AsignacionTable = ({ matrix, setMatrix }) => {
         }
     }, [columns]);
 
+    const importFromJSON = () => {
+        inputRef.current.click();
+    };
+
+    const fileChange = (e) => {
+        if(e.target.files) {
+            const fr = new FileReader();
+            fr.onload = () => {
+                const importedData = JSON.parse(fr.result);
+                setMatrix(importedData.matrix);
+                setRows(importedData.rows);
+                setColumns(importedData.columns);
+            }
+            fr.readAsText(e.target.files[0]);
+        }
+    }
+
+    const exportFile = () => {
+        const data = JSON.stringify({
+            matrix,
+            rows,
+            columns
+        });
+        const filename = "data.json";
+        const fileType = "text/json";
+
+        const blob = new Blob([data], { type: fileType });
+        const a = document.createElement("a");
+        a.download = filename;
+        a.href = window.URL.createObjectURL(blob);
+        const clickEvt = new MouseEvent("click", {
+            view: window,
+            bubbles: true,
+            cancelable: true,
+        });
+        a.dispatchEvent(clickEvt);
+        a.remove();
+    };
+
     return (
         <>
             <div className="table__container">
@@ -42,9 +90,30 @@ const AsignacionTable = ({ matrix, setMatrix }) => {
                         {matrix.map((row, indexI) => (
                             <tr className="table__row" key={"row" + indexI}>
                                 {row.map((value, indexJ) => {
-                                    return <td className="table__col" key={"col" + indexJ}>
-                                        <input className="table__input" type="text" onChange={(e) => handleChange(e, indexI, indexJ)} value={value} disabled={indexI === 0 && indexJ === 0 ? true : false} />
-                                    </td>;
+                                    return (
+                                        <td
+                                            className="table__col"
+                                            key={"col" + indexJ}
+                                        >
+                                            <input
+                                                className="table__input"
+                                                type="text"
+                                                onChange={(e) =>
+                                                    handleChange(
+                                                        e,
+                                                        indexI,
+                                                        indexJ
+                                                    )
+                                                }
+                                                value={value}
+                                                disabled={
+                                                    indexI === 0 && indexJ === 0
+                                                        ? true
+                                                        : false
+                                                }
+                                            />
+                                        </td>
+                                    );
                                 })}
                             </tr>
                         ))}
@@ -53,7 +122,23 @@ const AsignacionTable = ({ matrix, setMatrix }) => {
             </div>
             <div className="counter__container">
                 <Counter variable={rows} setVariable={setRows} text="Filas" />
-                <Counter variable={columns} setVariable={setColumns} text="Columnas" />
+                <Counter
+                    variable={columns}
+                    setVariable={setColumns}
+                    text="Columnas"
+                />
+                <Button onClick={exportFile} text="Exportar" />
+                <button onClick={importFromJSON}>
+                    Importar
+                    <input
+                        ref={inputRef}
+                        type="file"
+                        name="file"
+                        className="input--hidden"
+                        onChange={fileChange}
+                        accept="text/json"
+                    />
+                </button>
             </div>
         </>
     );
--- src/components/TransporteTable/TransporteTable.js
+++ src/components/TransporteTable/TransporteTable.js
@@ -1,12 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 import Counter from "../Counter/Counter";
+import Button from "../Button/Button";
 
 import "./TransporteTable.css";
 
 const TransporteTable = ({ matrix, setMatrix, available, setAvailable, demand, setDemand }) => {
     const [rows, setRows] = useState(3);
     const [columns, setColumns] = useState(3);
+
+    const inputRef = useRef(null);
 
     const handleChange = (e, i, j) => {
         setMatrix(matrix.map((elem, row) => 
@@ -56,6 +59,49 @@ const TransporteTable = ({ matrix, setMatrix, available, setAvailable, demand, s
         }
     }, [columns]);
 
+    const importFromJSON = () => {
+        inputRef.current.click();
+    };
+
+    const fileChange = (e) => {
+        if(e.target.files) {
+            const fr = new FileReader();
+            fr.onload = () => {
+                const importedData = JSON.parse(fr.result);
+                setMatrix(importedData.matrix);
+                setRows(importedData.rows);
+                setColumns(importedData.columns);
+                setAvailable(importedData.available);
+                setDemand(importedData.demand);
+            }
+            fr.readAsText(e.target.files[0]);
+        }
+    }
+
+    const exportFile = () => {
+        const data = JSON.stringify({
+            matrix,
+            available,
+            demand,
+            rows,
+            columns,
+        });
+        const filename = "data.json";
+        const fileType = "text/json";
+
+        const blob = new Blob([data], { type: fileType });
+        const a = document.createElement("a");
+        a.download = filename;
+        a.href = window.URL.createObjectURL(blob);
+        const clickEvt = new MouseEvent("click", {
+            view: window,
+            bubbles: true,
+            cancelable: true,
+        });
+        a.dispatchEvent(clickEvt);
+        a.remove();
+    };
+
     return (
         <>
             <div className="table__container">
@@ -91,6 +137,18 @@ const TransporteTable = ({ matrix, setMatrix, available, setAvailable, demand, s
             <div className="counter__container">
                 <Counter variable={rows} setVariable={setRows} text="Filas" />
                 <Counter variable={columns} setVariable={setColumns} text="Columnas"/>
+                <Button onClick={exportFile} text="Exportar" />
+                <button onClick={importFromJSON}>
+                    Importar
+                    <input
+                        ref={inputRef}
+                        type="file"
+                        name="file"
+                        className="input--hidden"
+                        onChange={fileChange}
+                        accept="text/json"
+                    />
+                </button>
             </div>
         </>
     );
